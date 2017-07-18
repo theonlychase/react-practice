0cd48ae90ef912a9a00d8cfbe07c8afcbaaeb3bb
Chase Isley <chaseisley@chases-mbp.attlocal.net>
2017-07-17 18:44:47 -0700

    Added to github

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+// comes with node
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry:'./app/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'index_bunde.js'
+	},
+	module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.html'
+    })
+  ]
+};
